c313b59d40a626a9dc5c8b5e23a33a036aae772a
Jatin Bhateja <jatin.bhateja@gmail.com>
2025-07-29 14:43:17 +0530

    Cleaups

--- jmh/run_jmh.sh
+++ jmh/run_jmh.sh
@@ -1,5 +1,6 @@
 
 BM=$1
 JSON=$2
+SIMD_JSON_DIR=/mnt/c/GitHub/simdjson-java/
 
-java -Xms8g -Xmx8g -XX:+UseParallelGC  --add-modules=jdk.incubator.vector -cp /mnt/c/GitHub/simdjson-java/build/libs/simdjson-java-0.3.1-SNAPSHOT.jar:./target/benchmarks.jar org.openjdk.jmh.Main ${BM} -f 1 -i 2 -wi 1 -w 30 -p fileName="${JSON}"
+java -Xms8g -Xmx8g -XX:+UseParallelGC  --add-modules=jdk.incubator.vector -cp ${SIMD_JSON_DIR}/build/libs/simdjson-java-0.3.1-SNAPSHOT.jar:./target/benchmarks.jar org.openjdk.jmh.Main ${BM} -f 1 -i 2 -wi 1 -w 30 -p fileName="${JSON}"
