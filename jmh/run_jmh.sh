f14042d864c3712e960cc7b9d98ec3a8ac215d29
Jatin Bhateja <jatinbha.cloud@gmail.com>
2025-08-05 21:13:15 +0530

    Optimizing Utf8Validator

--- jmh/run_jmh.sh
+++ jmh/run_jmh.sh
@@ -3,4 +3,4 @@ BM=$1
 JSON=$2
 SIMD_JSON_DIR=/mnt/c/GitHub/simdjson-java/
 
-java -Xms8g -Xmx8g -XX:+UseParallelGC  --add-modules=jdk.incubator.vector -cp ${SIMD_JSON_DIR}/build/libs/simdjson-java-0.3.1-SNAPSHOT.jar:./target/benchmarks.jar org.openjdk.jmh.Main ${BM} -f 1 -i 2 -wi 1 -w 30 -p fileName="${JSON}"
+java -Xms2g -Xmx4g -XX:+UseParallelGC  --add-modules=jdk.incubator.vector -cp ${SIMD_JSON_DIR}/build/libs/simdjson-java-0.3.1-jdk_24_changes-SNAPSHOT.jar:./target/benchmarks.jar org.openjdk.jmh.Main ${BM} -f 1 -i 2 -wi 1 -w 30 -p fileName="${JSON}"
